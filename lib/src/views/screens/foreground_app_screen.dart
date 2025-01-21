2d349854ff2b9703a0ee654783e9a4ea7b8b778b
Harkirat Singh <sandhukirat23@gmail.com>
2025-01-22 00:06:04 +1100

    feat: add current_app plugin

--- lib/src/views/screens/foreground_app_screen.dart
+++ lib/src/views/screens/foreground_app_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:current_app/current_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -9,38 +12,25 @@ class ForegroundAppScreen extends StatefulWidget {
 }
 
 class _ForegroundAppScreenState extends State<ForegroundAppScreen> {
-  static const platform = MethodChannel('com.example.just_think/foreground');
-  String _foregroundApp = "Unknown";
+  final _currentApp = CurrentApp();
 
-  Future<void> getForegroundApp() async {
+  void redirectToUsageAccessSettings() async {
     try {
-      final String result = await platform.invokeMethod('getForegroundApp');
-      setState(() {
-        _foregroundApp = result;
-      });
+      await _currentApp.redirectToUsageAccessSettings();
     } on PlatformException catch (e) {
-      setState(() {
-        _foregroundApp = "Failed to get foreground app: ${e.message}";
-      });
+      log("Failed to open app settings: ${e.message}");
     }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getForegroundApp();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Foreground App')),
-      body: Center(
-        child: Text('Current App: $_foregroundApp'),
+      appBar: AppBar(
+        title: Text('Foreground App'),
+        actions: [IconButton(onPressed: redirectToUsageAccessSettings, icon: Icon(Icons.settings))],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getForegroundApp,
-        child: Icon(Icons.refresh),
+      body: Center(
+        child: Text('Current App: null'),
       ),
     );
   }
